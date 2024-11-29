076327794456683d964238112cb5b2ce9a680e9f
Kent Slaymaker <kentslaymaker@gmail.com>
2024-11-28 20:36:59 -0700

    growing grid with zooming camera

--- arctic_2.py
+++ arctic_2.py
@@ -1,12 +1,15 @@
 from manim import *
 import random
 
-class Arctic(Scene):
+class Arctic(MovingCameraScene):
   def construct(self):
   
 ### Global Vars
-    a_n_max = 10
+    a_n_max = 5
+
     animation_speed = 1
+
+    #self.play(self.camera.frame.animate.set(width = 2).move_to(ORIGIN))
 
 ### Setup arctic array
     for a_n in range(1, a_n_max):
@@ -21,21 +24,25 @@ class Arctic(Scene):
             new_square = square.copy().shift([i, j, 0])
             #corner.add(Integer(j).next_to(new_square, RIGHT, -0.9))
             #corner.add(Integer(i).next_to(new_square, RIGHT, -0.5))
+            #corner.add(Integer(arctic[i][j]).next_to(new_square, RIGHT, -0.5))
             corner.add(new_square)
 
+      grid = Group(
+        corner,
+        corner.copy().shift([-a_n, 0, 0]).flip([0, 1, 0]),
+        corner.copy().shift([-a_n, -a_n, 0]).flip([-1, 1, 0]),
+        corner.copy().shift([0, -a_n, 0]).flip([1, 0, 0])
+      )
+
+      self.play(self.camera.auto_zoom(grid, margin=1))
       self.play(
         FadeIn(
-          Group(
-            corner,
-            corner.copy().shift([-a_n, 0, 0]).flip([0, 1, 0]),
-            corner.copy().shift([-a_n, -a_n, 0]).flip([-1, 1, 0]),
-            corner.copy().shift([0, -a_n, 0]).flip([1, 0, 0])
-          )
+          grid
         )
       )
 
 
 ### Wait
       self.wait(animation_speed)
-      self.clear()
+      #self.clear()
 
