dc4244ef38cee18d1e222d60dce37be9c4947f68
Kent Slaymaker <kentslaymaker@gmail.com>
2024-11-28 21:19:22 -0700

    performance pass and animation speedup

--- arctic_2.py
+++ arctic_2.py
@@ -5,9 +5,9 @@ class Arctic(MovingCameraScene):
   def construct(self):
   
 ### Global Vars
-    a_n_max = 5
+    a_n_max = 100
 
-    animation_speed = 1
+    animation_speed = 3
 
     #self.play(self.camera.frame.animate.set(width = 2).move_to(ORIGIN))
 
@@ -19,7 +19,7 @@ class Arctic(MovingCameraScene):
 
       for i in range(0, a_n):
         for j in range(0, a_n):
-          if i + j < a_n:
+          if i + j == a_n - 1:
             arctic[i][j] = 0
             new_square = square.copy().shift([i, j, 0])
             #corner.add(Integer(j).next_to(new_square, RIGHT, -0.9))
@@ -34,15 +34,16 @@ class Arctic(MovingCameraScene):
         corner.copy().shift([0, -a_n, 0]).flip([1, 0, 0])
       )
 
-      self.play(self.camera.auto_zoom(grid, margin=1))
       self.play(
+        self.camera.auto_zoom(grid, margin=1),
         FadeIn(
           grid
-        )
+        ),
+        run_time=animation_speed / a_n
       )
 
 
 ### Wait
-      self.wait(animation_speed)
+      self.wait(animation_speed / a_n)
       #self.clear()
 
